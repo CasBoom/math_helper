68e33c0670faf432b4892b052854ea508659f378
cas <casboomdtp@gmail.com>
2021-07-07 13:19:43 +0200

    create frontend

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+@section('title')
+Dashboard
+@endsection
+
+@section('content')
+    <form method="" action="post">
+        <h2>Calculus II trainer</h2>
+        @csrf
+        <input type="hidden" value="{{hash('ripemd160',now() . rand()) }}" name="id">
+        Name: <input type="text" name="name"><br>
+        <input type="submit" value="Start">
+    </form>
+@endsection
---
+++ resources/views/results.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+@section('title')
+Results
+@endsection
+
+@section('content')
+
+@endsection
